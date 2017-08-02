0a4775a3cbdef037ea3221917f474d48f529b8b6
zhyhang <zhyhang1978@gmail.com>
2017-08-02 23:19:24 +0800

    add bubble sort and test

--- algorithm/sort/sortalgorithm.go
+++ algorithm/sort/sortalgorithm.go
@@ -1,6 +1,14 @@
 package sort
 
-import (
+import ()
 
-)
-
+func Bubblesort(a []int) {
+	for i,change := 0,true; i < len(a) && change; i++ {
+		change=false
+		for j:= 1; j < len(a)-i; j++ {
+			if a[j] < a[j-1] {
+				a[j-1], a[j],change = a[j], a[j-1],true
+			}
+		}
+	}
+}
---
+++ algorithm/sort/sortalgorithm_test.go
@@ -0,0 +1,22 @@
+package sort
+
+import (
+	"testing"
+)
+
+func TestBubblesort(t *testing.T) {
+	cases := []struct {
+		in, want []int
+	}{
+		{[]int{3, 5, -1, 0, 6, 4}, []int{-1, 0, 3, 4, 5, 6}},
+		{[]int{-3, -2, -1, 0, 1, 2}, []int{-3, -2, -1, 0, 1, 2}},
+	}
+	for _, c := range cases {
+		Bubblesort(c.in)
+		for i := 0; i < len(c.in); i++ {
+			if c.in[i] != c.want[i] {
+				t.Errorf("Bubblesort(%v), want %v", c.in, c.want)
+			}
+		}
+	}
+}
