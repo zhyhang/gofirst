f5de40f46c51e3105438923882c29e6f05365315
zhyhang <zhyhang1978@gmail.com>
2017-08-03 22:47:18 +0800

    add test case

--- algorithm/sort/sortalgorithm_test.go
+++ algorithm/sort/sortalgorithm_test.go
@@ -10,6 +10,7 @@ func TestBubblesort(t *testing.T) {
 	}{
 		{[]int{3, 5, -1, 0, 6, 4}, []int{-1, 0, 3, 4, 5, 6}},
 		{[]int{-3, -2, -1, 0, 1, 2}, []int{-3, -2, -1, 0, 1, 2}},
+		{make([]int, 0, 0), make([]int, 0, 0)},
 	}
 	for _, c := range cases {
 		Bubblesort(c.in)
