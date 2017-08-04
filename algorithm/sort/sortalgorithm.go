7192c8d716d15ae28475cc89d489373cf4f4a982
zhyhang <zhyhang1978@gmail.com>
2017-08-04 18:32:14 +0800

    add quick sort methods

--- algorithm/sort/sortalgorithm.go
+++ algorithm/sort/sortalgorithm.go
@@ -3,12 +3,24 @@ package sort
 import ()
 
 func Bubblesort(a []int) {
-	for i,change := 0,true; i < len(a) && change; i++ {
-		change=false
-		for j:= 1; j < len(a)-i; j++ {
+	for i, change := 0, true; i < len(a) && change; i++ {
+		change = false
+		for j := 1; j < len(a)-i; j++ {
 			if a[j] < a[j-1] {
-				a[j-1], a[j],change = a[j], a[j-1],true
+				a[j-1], a[j], change = a[j], a[j-1], true
 			}
 		}
 	}
+}
+
+func Quicksort(a []int) {
+
+}
+
+func qsort(a []int, begin int, end int) {
+
+}
+
+func qsortpartition(a []int, begin int, end int) {
+
 }
