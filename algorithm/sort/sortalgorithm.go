b65fbd9a55dad271dd97a3ab6021e24ec5875aef
zhyhang <zhyhang1978@gmail.com>
2017-09-02 19:01:18 +0800

    add legancy merge sort algorithm

--- algorithm/sort/sortalgorithm.go
+++ algorithm/sort/sortalgorithm.go
@@ -46,3 +46,47 @@ func qsortPartition(a []int, begin int, end int) int {
 	}
 	return begin
 }
+
+func Mergesort(a []int) {
+	if len(a) == 0 {
+		return
+	}
+	aux := make([]int, len(a))
+	copy(aux, a)
+	msort(aux, a, 0, len(a)-1)
+}
+
+func msort(src, dest []int, s, t int) {
+	if s == t {
+		dest[s] = src[s]
+	} else {
+		m := (s + t) / 2
+		// Recursively sort
+		msort(dest, src, s, m)
+		msort(dest, src, m+1, t)
+		msMerge(src, dest, s, m, t)
+	}
+
+}
+
+// merge src[i,m],[m+1,n] to dest[i,max(m,n)]
+func msMerge(src, dest []int, i, m, n int) {
+	j, k := m+1, i
+	for ; i <= m && j <= n; k++ {
+		if src[i] <= src[j] {
+			dest[k] = src[i]
+			i++
+		} else {
+			dest[k] = src[j]
+			j++
+		}
+	}
+	for ; i <= m; i++ {
+		dest[k] = src[i]
+		k++
+	}
+	for ; j <= n; j++ {
+		dest[k] = src[j]
+		k++
+	}
+}
