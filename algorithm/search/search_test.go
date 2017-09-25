2871c951f69f98b4b120d27172a23f5166e09980
zhyhang <zhyhang1978@gmail.com>
2017-09-25 22:52:31 +0800

    add nearest search test

--- algorithm/search/search_test.go
+++ algorithm/search/search_test.go
@@ -47,3 +47,111 @@ func TestBinarySearchNest(t *testing.T) {
 		}
 	}
 }
+
+func TestBinarySearchNearest(t *testing.T) {
+
+	v := 0
+
+	//# root node is nil
+	result := BinarySearchNearest(nil, v)
+	if result != nil {
+		t.Errorf("BinarySearchNearest %d in nil, want nil, but return %v", v, result)
+	}
+
+	//# only one root node
+	root := &BiTreeNode{
+		nil, nil, 10,
+	}
+
+	// root.value==v
+	v = 10
+	result = BinarySearchNearest(root, v)
+	if result != root {
+		t.Errorf("BinarySearchNearest %d in %v, want %v, but return %v", v, root, root, result)
+	}
+
+	// root.value<v
+	v = 11
+	result = BinarySearchNearest(root, v)
+	if result != root {
+		t.Errorf("BinarySearchNearest %d in %v, want %v, but return %v", v, root, root, result)
+	}
+
+	// root.value>v
+	v = 4
+	result = BinarySearchNearest(root, v)
+	if result != root {
+		t.Errorf("BinarySearchNearest %d in %v, want %v, but return %v", v, root, root, result)
+	}
+
+	//# root has left child and right child
+	//
+	//				10
+	//		  8				12
+	//	7			9					14
+	//							13
+	//
+	left := &BiTreeNode{
+		nil, nil, 7,
+	}
+
+	right := &BiTreeNode{
+		nil, nil, 9,
+	}
+
+	rootLeft := &BiTreeNode{
+		left, right, 8,
+	}
+
+	left = &BiTreeNode{
+		nil, nil, 13,
+	}
+	right = &BiTreeNode{
+		left, nil, 14,
+	}
+
+	rootRight := &BiTreeNode{
+		nil, right, 12,
+	}
+
+	root = &BiTreeNode{
+		rootLeft, rootRight, 10,
+	}
+
+	// equal
+	for v = 7; v <= 10; v++ {
+		result = BinarySearchNearest(root, v)
+		if v != result.value {
+			t.Errorf("BinarySearchNearest %d in %v, want %d, but return %d", v, root, v, result.value)
+		}
+	}
+
+	// near
+	v = 11
+	result = BinarySearchNearest(root, v)
+	if result != rootRight {
+		t.Errorf("BinarySearchNearest %d in %v, want %v, but return %v", v, root, rootRight, result)
+	}
+
+	// near
+	v = 4
+	result = BinarySearchNearest(root, v)
+	if 7 != result.value {
+		t.Errorf("BinarySearchNearest %d in %v, want %d, but return %d", v, root, 7, result.value)
+	}
+
+	// near
+	v = 15
+	result = BinarySearchNearest(root, v)
+	if 14 != result.value {
+		t.Errorf("BinarySearchNearest %d in %v, want %d, but return %d", v, root, 15, result.value)
+	}
+
+	// equal
+	for v = 12; v <= 14; v++ {
+		result = BinarySearchNearest(root, v)
+		if v != result.value {
+			t.Errorf("BinarySearchNearest %d in %v, want %d, but return %d", v, root, v, result.value)
+		}
+	}
+}
