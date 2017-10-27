d56180acd3904de21633ed02ca9ac05f9cf2de37
zhyhang <zhyhang1978@gmail.com>
2017-10-27 22:52:42 +0800

    complete tree unit test

--- algorithm/tree/tree_test.go
+++ algorithm/tree/tree_test.go
@@ -5,7 +5,89 @@ import (
 )
 
 func TestMirrorBinaryTree(t *testing.T) {
-
+	// nil tree
+	root := NewBinaryTreeNode("", "", "")
+	MirrorBinaryTree(root)
+	rootStr := TravelBinaryTree(root)
+	if "" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "nil", rootStr)
+	}
+	// most 3 nodes
+	cases := []struct {
+		o, l, r, want string
+	}{
+		{"1", "2", "3", "1,3,2"},
+		{"1", "", "3", "1,3"},
+		{"1", "2", "", "1,2"},
+	}
+	for _, c := range cases {
+		root = NewBinaryTreeNode(c.o, c.l, c.r)
+		MirrorBinaryTree(root)
+		rootStr = TravelBinaryTree(root)
+		if c.want != rootStr {
+			t.Errorf("MirrorBinaryTree, want %s, but %s", c.want, rootStr)
+		}
+	}
+	// only have many left nodes
+	root = NewBinaryTreeNode("1", "2", "")
+	node := NewBinaryTreeNode("10", "20", "")
+	root.left.left = node
+	node.left.left = NewBinaryTreeNode("100", "200", "")
+	MirrorBinaryTree(root)
+	rootStr = TravelBinaryTree(root)
+	if "1,2,10,20,100,200" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "1,2,10,20,100,200", rootStr)
+	}
+	// only have many right nodes
+	root = NewBinaryTreeNode("1", "", "3")
+	node = NewBinaryTreeNode("10", "", "30")
+	root.right.right = node
+	node.right.right = NewBinaryTreeNode("100", "", "300")
+	MirrorBinaryTree(root)
+	rootStr = TravelBinaryTree(root)
+	if "1,3,10,30,100,300" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "1,3,10,30,100,300", rootStr)
+	}
+	// height=3, full binary tree (lack of right in bottom level)
+	root = NewBinaryTreeNode("1", "2", "3")
+	node = NewBinaryTreeNode("10", "20", "30")
+	root.left = node
+	MirrorBinaryTree(root)
+	rootStr = TravelBinaryTree(root)
+	if "1,3,10,30,20" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "1,3,10,30,20", rootStr)
+	}
+	// height=3, full binary tree (lack of left in bottom level)
+	root = NewBinaryTreeNode("1", "2", "3")
+	node = NewBinaryTreeNode("10", "20", "30")
+	root.right = node
+	MirrorBinaryTree(root)
+	rootStr = TravelBinaryTree(root)
+	if "1,10,30,20,2" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "1,10,30,20,2", rootStr)
+	}
+	// height=3, complete tree
+	root = NewBinaryTreeNode("1", "2", "3")
+	node = NewBinaryTreeNode("2", "21", "22")
+	root.left = node
+	node = NewBinaryTreeNode("3", "31", "32")
+	root.right = node
+	MirrorBinaryTree(root)
+	rootStr = TravelBinaryTree(root)
+	if "1,3,32,31,2,22,21" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "1,3,32,31,2,22,21", rootStr)
+	}
+	// any tree
+	root = NewBinaryTreeNode("1", "2", "5")
+	node = NewBinaryTreeNode("2", "", "3")
+	root.left = node
+	root.right = NewBinaryTreeNode("5", "6", "7")
+	node.right = NewBinaryTreeNode("3", "4", "")
+	MirrorBinaryTree(root)
+	rootStr = TravelBinaryTree(root)
+	if "1,5,7,6,2,3,4" != rootStr {
+		t.Errorf("MirrorBinaryTree, want %s, but %s", "1,5,7,6,2,3,4", rootStr)
+	}
 }
 
 func TestTravelBinaryTree(t *testing.T) {
