48ed278d31b9d8b0dd51d430994694134279509a
zhyhang <zhyhang1978@gmail.com>
2017-11-20 20:06:21 +0800

    update time format/parse example

--- example/study/base/time/fmtparse.go
+++ example/study/base/time/fmtparse.go
@@ -15,56 +15,139 @@ func format() [][]string {
 	t := time.Now()
 	result := make([][]string, 24)
 	i := 0
-	makeformatdata(result, i, "UnixDate: ", t.Format(time.UnixDate))
+	makeresult(result, i, "UnixDate: ", t.Format(time.UnixDate))
 	i++
-	makeformatdata(result, i, "ANSIC:", t.Format(time.ANSIC))
+	makeresult(result, i, "ANSIC:", t.Format(time.ANSIC))
 	i++
-	makeformatdata(result, i, "RFC3339:", t.Format(time.RFC3339))
+	makeresult(result, i, "RFC3339:", t.Format(time.RFC3339))
 	i++
-	makeformatdata(result, i, "StampMilli:", t.Format(time.StampMilli))
+	makeresult(result, i, "StampMilli:", t.Format(time.StampMilli))
 	i++
-	makeformatdata(result, i, "Common:", t.Format("2006-01-02 15:04:05"))
+	makeresult(result, i, "Common:", t.Format("2006-01-02 15:04:05"))
 	i++
-	makeformatdata(result, i, "CommonSimple:", t.Format("20060102150405"))
+	makeresult(result, i, "CommonPM:", t.Format("2006-01-02 03:04:05PM"))
 	i++
-	makeformatdata(result, i, "CommonMilli:", t.Format("2006-01-02 15:04:05.000"))
+	makeresult(result, i, "CommonPMOffset:", t.Format("2006-01-02 03:04:05PMZ0700"))
+	i++
+	makeresult(result, i, "CommonPMOffsetLoc:", t.Format("2006-01-02 03:04:05PM -0700 MST"))
+	i++
+	makeresult(result, i, "CommonSimple:", t.Format("20060102150405"))
+	i++
+	makeresult(result, i, "CommonMilli:", t.Format("2006-01-02 15:04:05.000"))
 	i++
 	millifmt := strconv.Itoa(1000 + t.Nanosecond()/1000000)[1:]
-	makeformatdata(result, i, "CommonMilliWithoutPeriod:", t.Format("2006-01-02 15:04:05")+" "+millifmt)
+	makeresult(result, i, "CommonMilliWithoutPeriod:", t.Format("2006-01-02 15:04:05")+" "+millifmt)
 	i++
-	makeformatdata(result, i, "CommonMilliSimple:", t.Format("20060102150405.000"))
+	makeresult(result, i, "CommonMilliSimple:", t.Format("20060102150405.000"))
 	i++
-	makeformatdata(result, i, "CommonMilliSimpleWithoutPeriod:", t.Format("20060102150405")+millifmt)
+	makeresult(result, i, "CommonMilliSimpleWithoutPeriod:", t.Format("20060102150405")+millifmt)
 	return result
 }
 
-func makeformatdata(result [][]string, i int, l, ls string) {
+func makeresult(result [][]string, i int, l, ls string) {
 	result[i] = make([]string, 2)
 	result[i][0] = l
 	result[i][1] = ls
 }
 
 // examples of parsing time from string by layout
-func parse() map[string]time.Time{
-	result := make(map[string]time.Time)
+func parse() [][]string {
+	result := make([][]string, 128)
+	i := 0
 
 	// parse local time
-	timestr:="2017-11-18 23:00:08"
-	l:="2006-01-02 15:04:05"
-	t,_:=time.ParseInLocation(l, timestr, time.Local)
-	result["layout: " +l+"; local time: "+timestr]=t
-	timestr="2017-11-18 23:00:08.666"
-	l="2006-01-02 15:04:05"
-	t,_=time.ParseInLocation(l, timestr, time.Local)
-	result["layout: " +l+"; local time: "+timestr]=t
-	return result
+	// when time string include location info, will using it
+	timestr := "2017-11-18 23:00:08"
+	l := "2006-01-02 15:04:05"
+	t, _ := time.ParseInLocation(l, timestr, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
 
-	// parse utc time
+	timestr = "2017-11-18 23:00:08.666"
+	l = "2006-01-02 15:04:05" // layout can put .000 or not put .000, for parsing millis
+	t, _ = time.ParseInLocation(l, timestr, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
+
+	timestr = "20171118230008.666"
+	l = "20060102150405.000" // layout can put .000 or not put .000, for parsing millis
+	t, _ = time.ParseInLocation(l, timestr, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
+
+	timestr = "20171118230008666"
+	timestr1 := timestr[0:14] + "." + timestr[14:]
+	l = "20060102150405.000"
+	t, _ = time.ParseInLocation(l, timestr1, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
+
+	timestr = "Sat Nov 18 2017 23:00:08.666 CST +0800"
+	l = "Mon Jan _2 2006 15:04:05.000 MST Z0700"
+	t, _ = time.ParseInLocation(l, timestr, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
+
+	timestr = "Sat Nov 18 2017 23:00:08.666 MST" //include location info using it
+	l = "Mon Jan _2 2006 15:04:05.000 MST"
+	t, _ = time.ParseInLocation(l, timestr, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
+	i++
+	makeresult(result, i, "layout: "+l+"; local time(to local): "+timestr, t.Local().String())
+
+	// parse utc time [parse() method default logic]
+	// when time string include location info, will using it
+	timestr = "2017-11-18 23:00:08"
+	l = "2006-01-02 15:04:05"
+	t, _ = time.Parse(l, timestr)
+	i++
+	makeresult(result, i, "layout: "+l+"; utc time: "+timestr, t.String())
+
+	timestr = "2017-11-18 23:00:08.666"
+	l = "2006-01-02 15:04:05" // layout can put .000 or not put .000, for parsing millis
+	t, _ = time.Parse(l, timestr)
+	i++
+	makeresult(result, i, "layout: "+l+"; utc time: "+timestr, t.String())
+
+	timestr = "20171118230008.666"
+	l = "20060102150405.000" // layout can put .000 or not put .000, for parsing millis
+	t, _ = time.Parse(l, timestr)
+	i++
+	makeresult(result, i, "layout: "+l+"; utc time: "+timestr, t.String())
+
+	timestr = "20171118230008666"
+	timestr1 = timestr[0:14] + "." + timestr[14:]
+	l = "20060102150405.000"
+	t, _ = time.Parse(l, timestr1)
+	i++
+	makeresult(result, i, "layout: "+l+"; utc time: "+timestr, t.String())
+
+	timestr = "Sat Nov 18 2017 23:00:08.666 CST +0800" //include location info, using it
+	l = "Mon Jan _2 2006 15:04:05.000 MST Z0700"
+	t, _ = time.Parse(l, timestr)
+	i++
+	makeresult(result, i, "layout: "+l+"; utc time: "+timestr, t.String())
+	i++
+	makeresult(result, i, "layout: "+l+"; utc time(to utc): "+timestr, t.UTC().String())
+
+	// time string is error
+	timestr = "20171118230008.abc"
+	l = "20060102150405.000"
+	t, _ = time.ParseInLocation(l, timestr, time.Local)
+	i++
+	makeresult(result, i, "layout: "+l+"; local time: "+timestr, t.String())
+
+	return result
 }
 
 // local time from timestamp (millis)
 func parseLocaltimeOfTimestamp(millis int64) time.Time {
-	return time.Unix(millis/1000, 1000000*(millis%1000))
+	nsec := millis % 1000
+	if nsec < 0 {
+		nsec = -nsec
+	}
+	return time.Unix(millis/1000, 1000000*nsec)
 }
 
 // recommend using this method to get milliseconds
@@ -89,36 +172,57 @@ func toTimestampMillis2(t time.Time) int64 {
 
 func main() {
 
-	print := fmt.Println
+	prtln := fmt.Println
 
-	print("time now:")
+	prtln("time now:")
 	tnow := timenow()
-	print(tnow)
+	prtln("\t" + tnow.String())
 
-	print()
-	print("date time format:")
-	for _, fmttime := range format() {
-		if len(fmttime) > 0 {
-			print("\t" + fmttime[0] + "\t" + fmttime[1])
+	prtln()
+	prtln("date time format:")
+	for i, v := range format() {
+		if len(v) > 0 {
+			prtln("\t" + strconv.Itoa(i+1) + "." + v[0] + "\t" + v[1])
 		}
 	}
 
-	print()
-	print("date time parse:")
-	for k,v:=range parse(){
-		print(k+" ---> "+v.String())
+	prtln()
+	prtln("date time parse:")
+	for i, v := range parse() {
+		if len(v) > 0 {
+			prtln("\t" + strconv.Itoa(i) + "." + v[0] + " ---> " + v[1])
+		}
 	}
 
-	print()
-	print("currrent time millis:")
-	print("\tmethod1: " + strconv.FormatInt(toTimestampMillis1(tnow), 10))
-	print("\tmehtod2: " + strconv.FormatInt(toTimestampMillis2(tnow), 10))
+	prtln()
+	prtln("currrent time millis:")
+	prtln("\tmethod1: " + strconv.FormatInt(toTimestampMillis1(tnow), 10))
+	prtln("\tmehtod2: " + strconv.FormatInt(toTimestampMillis2(tnow), 10))
 
-	print("date time millis:")
-	longt, _ := time.ParseInLocation("2006-01-02 15:04:05", "1978-12-20 11:00:12.888", time.Local)
-	print("\tmethod1: " + strconv.FormatInt(toTimestampMillis1(longt), 10))
-	print("\tmehtod2: " + strconv.FormatInt(toTimestampMillis2(longt), 10))
+	prtln()
+	prtln("date time millis:")
+	longt, _ := time.ParseInLocation("2006-01-02 15:04:05", "2017-11-17 18:28:38.888", time.Local)
+	prtln("\ttime is " + longt.String())
+	prtln("\t\tmethod1: " + strconv.FormatInt(toTimestampMillis1(longt), 10))
+	prtln("\t\tmehtod2: " + strconv.FormatInt(toTimestampMillis2(longt), 10))
+	longt, _ = time.ParseInLocation("2006-01-02 15:04:05", "0025-10-28 08:28:38.666", time.Local)
+	prtln("\ttime is " + longt.String())
+	prtln("\t\tmethod1: " + strconv.FormatInt(toTimestampMillis1(longt), 10))
+	prtln("\t\tmehtod2: " + strconv.FormatInt(toTimestampMillis2(longt), 10))
+	longt, _ = time.ParseInLocation("2006-01-02 15:04:05", "2888-02-21 08:18:28.666", time.Local)
+	prtln("\ttime is " + longt.String())
+	prtln("\t\tmethod1: " + strconv.FormatInt(toTimestampMillis1(longt), 10))
+	prtln("\t\tmehtod2: " + strconv.FormatInt(toTimestampMillis2(longt), 10))
 
-	print(time.Unix(282970812, 888000000))
+	prtln()
+	prtln("data time parse from millis timestamp:")
+	ts := int64(-61352293025666) //0025-10-28 08:28:38.666
+	prtln("\ttimestamp millis: " + strconv.FormatInt(ts, 10) + "; to time: " + parseLocaltimeOfTimestamp(ts).String())
+	ts = int64(1510914518888) //2017-11-17 18:28:38.888
+	prtln("\ttimestamp millis: " + strconv.FormatInt(ts, 10) + "; to time: " + parseLocaltimeOfTimestamp(ts).String())
+	ts = int64(1511164435560)
+	prtln("\ttimestamp millis: " + strconv.FormatInt(ts, 10) + "; to time: " + parseLocaltimeOfTimestamp(ts).String())
+	ts = int64(28973722708666) //2888-02-21 08:18:28.666
+	prtln("\ttimestamp millis: " + strconv.FormatInt(ts, 10) + "; to time: " + parseLocaltimeOfTimestamp(ts).String())
 
 }
