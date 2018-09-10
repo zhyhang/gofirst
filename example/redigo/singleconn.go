f19f20f91b1328bd6a0f5b7e0df3755dbf7b4000
zhyhang <zhyhang1978@gmail.com>
2018-09-10 23:20:27 +0800

    add redigo redis golang client single connection example

---
+++ example/redigo/singleconn.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+)
+
+func main() {
+	conn, err := redis.Dial("tcp", ":6400")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	result, err := redis.String(conn.Do("info"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
