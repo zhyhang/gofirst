a8612e9d207fe24677f1a3a799c929b0f7f9629f
zhyhang <zhyhang1978@gmail.com>
2019-01-23 11:22:48 +0800

    example: create field through api

--- example/pilosa/wrapperPilosa.go
+++ example/pilosa/wrapperPilosa.go
@@ -35,20 +35,24 @@ func main() {
 			fmt.Println(field.Type())
 		}
 	}
-	// directly read index and filed from pilosa Server
-	holder := pilosaServer.Holder()
-	index := holder.Index("did")
-	fields := index.Fields()
-	for _, field := range fields {
-		fmt.Println(field.Name())
-	}
 
 	// create index
 	newIndexName := "wrapper-index-1"
 
 	if newIndex(api, newIndexName) {
-		log.Printf("create index %s success\n", newIndexName)
+		log.Printf("create index %s success", newIndexName)
 	}
+
+	// directly read index and filed from pilosa Server
+	holder := pilosaServer.Holder()
+	index := holder.Index(newIndexName)
+	fields := index.Fields()
+	log.Printf("fields of index %s:", newIndexName)
+	fieldStr := "\t"
+	for _, field := range fields {
+		fieldStr = fieldStr + field.Name() + "\t"
+	}
+	log.Println(fieldStr)
 
 	// blocking for pilosaServer alive
 	serverCmd.Wait()
@@ -60,7 +64,19 @@ func newIndex(api *pilosa.API, name string) bool {
 	_, err := api.CreateIndex(context.Background(), name, options)
 	success := true
 	if err != nil {
-		log.Printf("create index %s error, because: %v", name, err)
+		log.Printf("create index %s error, %v", name, err)
+		success = false
+	}
+	newField(api, name, "field1")
+	newField(api, name, "field2")
+	return success
+}
+
+func newField(api *pilosa.API, indexName, fieldName string) bool {
+	success := true
+	_, err := api.CreateField(context.Background(), indexName, fieldName)
+	if err != nil {
+		log.Printf("create field %s error, %v", fieldName, err)
 		success = false
 	}
 	return success
