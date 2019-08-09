4b6ebda306b7e3cea5d2be0bcb28dfbc52791059
Zhao Yuhang <zhyhang1978@gmail.com>
2019-08-09 15:15:56 +0800

    Update oracleconst.go

--- example/study/db/dbconst/oracleconst.go
+++ example/study/db/dbconst/oracleconst.go
@@ -3,5 +3,5 @@ package dbconst
 const (
 	MsgDsn = `Please specifiy connection parameter in GO_OCI8_CONNECT_STRING environment variable,
 or as the first argument! (The format is user/pass@host:port/sid)`
-	DefaultDsn = "sys/Ipinyou.com2017@127.0.0.1/orcltest?as=sysdba"
+	DefaultDsn = "sys/sys@127.0.0.1/orcltest?as=sysdba"
 )
